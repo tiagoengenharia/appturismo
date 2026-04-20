547365a5dfe119a0f758313a09403348df5af853
tiago.oliveira <tiago.oliveira@samsung.com>
2026-04-19 23:18:53 -0300

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>teste</div>
+    <div>
+      <Button>teste</Button>
+    </div>
   );
 }
