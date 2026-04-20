b6d838b319e449c43668569811ad524a9f4ffca4
tiago.oliveira <tiago.oliveira@samsung.com>
2026-04-20 15:15:20 -0300

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,47 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 
 export default function Home() {
   return (
-    <div>
-      <Button>teste</Button>
+    <><div>
+      <Accordion
+        type="single"
+        collapsible
+        defaultValue="shipping"
+        className="max-w-lg"
+      >
+        <AccordionItem value="shipping">
+          <AccordionTrigger>Elisa</AccordionTrigger>
+          <AccordionContent>
+            Elisa eh uma menina mto doida, sabe desenhar e gosta de matematica.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="returns">
+          <AccordionTrigger>What is your return policy?</AccordionTrigger>
+          <AccordionContent>
+            Returns accepted within 30 days. Items must be unused and in original
+            packaging. Refunds processed within 5-7 business days.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="support">
+          <AccordionTrigger>How can I contact customer support?</AccordionTrigger>
+          <AccordionContent>
+            Reach us via email, live chat, or phone. We respond within 24 hours
+            during business days.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
+    <div>
+        <Button>Brincar</Button>
+        <Button>Brincar</Button>
+        <Button>Brincar</Button>
+      </div></>
   );
 }
